f5bde473274e6ce57cf7291b0cda53f83d54e2f3
Orlando <95863162+lalomax@users.noreply.github.com>
2022-05-31 14:09:01 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,18 @@
+
+// console.log(allNamesDOMCollection)
+
+
+document.getElementById("searchInput").addEventListener("keyup", (event) => {
+  let searchQuery = event.target.value.toLowerCase()
+  let allNamesDOMCollection = document.getElementsByClassName("name")
+
+  for (let i = 0; i < allNamesDOMCollection.length; i++) {
+    const currentName = allNamesDOMCollection[i].textContent.toLowerCase()
+    if (currentName.includes(searchQuery)) {
+      allNamesDOMCollection[i].style.display = "block"
+    } else  {
+      allNamesDOMCollection[i].style.display = "none"
+    }
+  }
+  console.log(searchQuery)
+})
